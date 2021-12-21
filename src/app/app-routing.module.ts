cd166b7037b4f2334bfce56663d12a129ed92905
mahmoud abdelwahab <mahmoudabdelwahab4242@gmail.com>
2021-12-21 14:28:13 +0200

    move all main pages to pages folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/component/home.component';
+import { HomeComponent } from './pages/home/component/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
@@ -9,17 +9,17 @@ const routes: Routes = [
   {
     path: 'Orders',
     loadChildren: () =>
-      import('./orders/orders.module').then((m) => m.OrdersModule),
+      import('./pages/orders/orders.module').then((m) => m.OrdersModule),
   },
   {
     path: 'About',
     loadChildren: () =>
-      import('./about/about.module').then((m) => m.AboutModule),
+      import('./pages/about/about.module').then((m) => m.AboutModule),
   },
   {
     path: 'Login',
     loadChildren: () =>
-      import('./login/login.module').then((m) => m.LoginModule),
+      import('./pages/login/login.module').then((m) => m.LoginModule),
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/component/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MaterialModule } from './material.module';
-import { HomeComponent } from './home/component/home.component';
-import { CustomersComponent } from './home/component/customers/customers.component';
-import { CardViewComponent } from './home/component/customers/card-view/card-view.component';
-import { ListViewComponent } from './home/component/customers/list-view/list-view.component';
-import { MapViewComponent } from './home/component/customers/map-view/map-view.component';
-import { NewCustomerComponent } from './home/component/customers/new-customer/new-customer.component';
 import { AgmCoreModule } from '@agm/core';
 import { environment } from 'src/environments/environment.prod';
-import { CustomerInformationComponent } from './home/component/customer-information/customer-information.component';
-import { DetailsComponent } from './home/component/customer-information/details/details.component';
-import { EditComponent } from './home/component/customer-information/edit/edit.component';
-import { OrdersComponent } from './home/component/customer-information/orders/orders.component';
+
+import { HeaderComponent } from './pages/header/component/header.component';
+import { CustomersComponent } from './pages/home/component/customers/customers.component';
+import { CardViewComponent } from './pages/home/component/customers/card-view/card-view.component';
+import { ListViewComponent } from './pages/home/component/customers/list-view/list-view.component';
+import { MapViewComponent } from './pages/home/component/customers/map-view/map-view.component';
+import { NewCustomerComponent } from './pages/home/component/customers/new-customer/new-customer.component';
+import { HomeComponent } from './pages/home/component/home.component';
+import { CustomerInformationComponent } from './pages/home/component/customer-information/customer-information.component';
+import { DetailsComponent } from './pages/home/component/customer-information/details/details.component';
+import { OrdersComponent } from './pages/home/component/customer-information/orders/orders.component';
+import { EditComponent } from './pages/home/component/customer-information/edit/edit.component';
 
 @NgModule({
   declarations: [
